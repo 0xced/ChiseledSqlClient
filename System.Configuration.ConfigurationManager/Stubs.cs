82f487798a9622a1db84c821144f1a088bf1ebe6
Cédric Luthi <cedric.luthi@gmail.com>
2024-03-01 14:33:58 +0100

    System.Configuration.ConfigurationManager stub

---
+++ System.Configuration.ConfigurationManager/Stubs.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+
+namespace System.Configuration
+{
+    public class ConfigurationElementCollection : IEnumerable
+    {
+        public int Count => 0;
+
+        public IEnumerator GetEnumerator()
+        {
+            yield break;
+        }
+    }
+
+    public class ConfigurationErrorsException
+    {
+    }
+
+    public class ConfigurationManager
+    {
+        public static object GetSection(string sectionName) => null;
+    }
+
+    public class ConfigurationSection
+    {
+    }
+
+    public class ProviderSettings
+    {
+        public string Name => null;
+        public string Type => null;
+    }
+
+    public class ProviderSettingsCollection
+    {
+    }
+}
